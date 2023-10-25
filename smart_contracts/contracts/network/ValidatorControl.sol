23d429ad9aac7f507f083bfdf1aff703707121b2
aziz.karabashov <aziz.karabashov@dsr-corporation.com>
2023-10-25 15:16:21 +0500

    Update validator control docs
    Signed-off-by: aziz.karabashov <aziz.karabashov@dsr-corporation.com>

--- smart_contracts/contracts/network/ValidatorControl.sol
+++ smart_contracts/contracts/network/ValidatorControl.sol
@@ -46,19 +46,17 @@ contract ValidatorControl is ValidatorSmartContractInterface, ControlledUpgradea
     mapping(address validatorAddress => ValidatorInfo validatorInfo) private validatorInfos;
 
     /**
-     * @dev Reference to the contract managing auth permissions
-         */
-    RoleControlInterface private roleControl;
-
-    /**
-     * @dev Modifier that checks that an the sender account has Steward role assigned.
+     * @dev Modifier that checks that the sender account has Steward role assigned
      */
     modifier _senderIsSteward() {
         if (!_roleControl.hasRole(RoleControlInterface.ROLES.STEWARD, msg.sender)) revert Unauthorized(msg.sender);
         _;
     }
 
-    modifier nonZeroValidatorAddress(address validator) {
+    /**
+     * @dev Modifier that checks that the validator address is not zero
+     */
+    modifier _nonZeroValidatorAddress(address validator) {
         if (validator == address(0)) revert InvalidValidatorAddress();
          _;
     }
@@ -88,8 +86,18 @@ contract ValidatorControl is ValidatorSmartContractInterface, ControlledUpgradea
         return validators;
     }
 
-    /**
-     * @dev Add a new validator to the list
+   /**
+     * @dev Adds a new validator to the list.
+     * 
+     * Restrictions:
+     * - Only accounts with the steward role are permitted to call this method; otherwise, will revert with an `Unauthorized` error
+     * - The validator address must be non-zero; otherwise, will revert with an `InvalidValidatorAddress` error
+     * - The total number of validators must not exceed 256; otherwise, will revert with an `ExceedsValidatorLimit` error
+     * - The validator must not already exist in the list; otherwise, a `ValidatorAlreadyExists` error will be thrown
+     * - The sender of the transaction must not have an active validator; otherwise, will revert with a `SenderHasActiveValidator` error
+     * 
+     * Events:
+     * - On successful validator creation, will emit a `ValidatorAdded` event
      */
     function addValidator(address newValidator) external senderIsSteward nonZeroValidatorAddress(newValidator) {
         if (validators.length >= MAX_VALIDATORS) revert ExceedsValidatorLimit(MAX_VALIDATORS);
@@ -110,6 +118,15 @@ contract ValidatorControl is ValidatorSmartContractInterface, ControlledUpgradea
 
     /**
      * @dev Remove an existing validator from the list
+     * 
+     * Restrcitions:
+     * - Only accounts with the steward role are permitted to call this method; otherwise, will revert with an `Unauthorized` error
+     * - The validator address must be non-zero; otherwise, will revert with an `InvalidValidatorAddress` error
+     * - The validator must not be last one; otherwise, will revert with an `CannotDeactivateLastValidator` error
+     * - The validator must exist; otherwise, will revert with an `ValidatorNotFound` error
+     * 
+     * Events:
+     * - On successful validator removal, will emit a `ValidatorRemoved` event
      */
     function removeValidator(address validator) external senderIsSteward nonZeroValidatorAddress(validator) {
         if (validators.length == 1) revert CannotDeactivateLastValidator();
