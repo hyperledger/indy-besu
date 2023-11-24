4cf1b4df1cc8ac217f853de935dbd5c01aba58de
artem.ivanov <artem.ivanov@dsr-corporation.com>
2023-11-24 15:51:01 +0300

    Added parent CLRegistry contract for Schema and Credential Definition
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-corporation.com>

--- indy-besu/smart_contracts/contracts/cl/CredentialDefinitionRegistry.sol
+++ indy-besu/smart_contracts/contracts/cl/CredentialDefinitionRegistry.sol
@@ -11,18 +11,14 @@ import { CredentialDefinition, CredentialDefinitionWithMetadata } from "./Creden
 import { CredentialDefinitionRegistryInterface } from "./CredentialDefinitionRegistryInterface.sol";
 import { CredentialDefinitionValidator } from "./CredentialDefinitionValidator.sol";
 import { CredentialDefinitionAlreadyExist, CredentialDefinitionNotFound, IssuerHasBeenDeactivated, IssuerNotFound, SenderIsNotIssuerDidOwner } from "./ClErrors.sol";
+import { CLRegistry } from "./CLRegistry.sol";
 import { SchemaRegistryInterface } from "./SchemaRegistryInterface.sol";
 import { toSlice } from "@dk1a/solidity-stringutils/src/StrSlice.sol";
 
 using CredentialDefinitionValidator for CredentialDefinition;
 using { toSlice } for string;
 
-contract CredentialDefinitionRegistry is CredentialDefinitionRegistryInterface, ControlledUpgradeable {
-    /**
-     * @dev Reference to the contract that manages DIDs
-     */
-    DidRegistryInterface private _didRegistry;
-
+contract CredentialDefinitionRegistry is CredentialDefinitionRegistryInterface, ControlledUpgradeable, CLRegistry {
     /**
      * @dev Reference to the contract that manages anoncreds schemas
      */
@@ -57,25 +53,11 @@ contract CredentialDefinitionRegistry is CredentialDefinitionRegistryInterface, 
         _;
     }
 
-    /**
-     * Checks that the Issuer DID exist, controlled by sender, and active
-     */
-    modifier _validIssuer(string memory id) {
-        try _didRegistry.resolveDid(id) returns (DidDocumentStorage memory didDocumentStorage) {
-            if (msg.sender != didDocumentStorage.metadata.creator)
-                revert SenderIsNotIssuerDidOwner(msg.sender, didDocumentStorage.metadata.creator);
-            if (didDocumentStorage.metadata.deactivated) revert IssuerHasBeenDeactivated(id);
-            _;
-        } catch (bytes memory reason) {
-            if (Errors.equals(reason, DidNotFound.selector)) {
-                revert IssuerNotFound(id);
-            }
-
-            Errors.rethrow(reason);
-        }
-    }
-
-    constructor(address didRegistryAddress, address schemaRegistryAddress) {
+    function initialize(
+        address didRegistryAddress,
+        address schemaRegistryAddress,
+        address upgradeControlAddress
+    ) public reinitializer(1) {
         _didRegistry = DidRegistryInterface(didRegistryAddress);
         _schemaRegistry = SchemaRegistryInterface(schemaRegistryAddress);
         _initializeUpgradeControl(upgradeControlAddress);
