57fc7c8d1512498939fc2e864eded7fe3d9d5b61
artem.ivanov <artem.ivanov@dsr-corporation.com>
2024-01-12 09:01:44 +0300

    Removed temp methods
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-corporation.com>

--- indy-besu/vdr/src/utils/mod.rs
+++ indy-besu/vdr/src/utils/mod.rs
@@ -2,11 +2,3 @@ mod common;
 
 #[cfg(test)]
 pub use common::{init_env_logger, rand_bytes, rand_string};
-
-use crate::{ContractParam, VdrResult};
-
-pub fn encode_contract_param<T: Into<ContractParam>>(obj: T) -> VdrResult<Vec<u8>> {
-    let contract_param: ContractParam = obj.into();
-    let encoded = ethabi::encode(&[contract_param]);
-    Ok(encoded)
-}
