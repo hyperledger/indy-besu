52e243db84e9fd711a1437370b871f522f09dd46
DenisRybas <denis.rybas@dsr-corporation.com>
2024-02-08 18:48:54 +0300

    removed warnings
    Signed-off-by: DenisRybas <denis.rybas@dsr-corporation.com>

--- vdr/src/contracts/cl/mod.rs
+++ vdr/src/contracts/cl/mod.rs
@@ -2,12 +2,4 @@ pub mod credential_definition_registry;
 pub mod schema_registry;
 pub mod types;
 
-pub use types::{
-    schema::{Schema, SchemaCreatedEvent},
-    schema_id::SchemaId,
-};
 
-pub use types::{
-    credential_definition::{CredentialDefinition, CredentialDefinitionCreatedEvent},
-    credential_definition_id::CredentialDefinitionId,
-};
--- vdr/src/contracts/network/mod.rs
+++ vdr/src/contracts/network/mod.rs
@@ -1,5 +1,3 @@
 pub mod validator_control;
 pub mod validator_info;
 
-pub use validator_control::*;
-pub use validator_info::*;
