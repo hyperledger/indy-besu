800bbfa27557512e40e1e25844db62d095ac5455
Rafael Bays Weiler <rafael.weiler@serpro.gov.br>
2025-02-10 09:13:06 -0300

    #108 Revocation Registry: vdr

--- vdr/src/contracts/anoncreds/types/revocation_registry_events.rs
+++ vdr/src/contracts/anoncreds/types/revocation_registry_events.rs
@@ -19,10 +19,8 @@ pub struct RevRegEntryCreated {
 #[serde(rename_all = "camelCase")]
 pub struct RevRegEntry {
     pub current_accumulator: String,
-    pub prev_accumulator: String,
     pub issued: Vec<u32>,
     pub revoked: Vec<u32>,
-    pub timestamp: u64,
 }
 
 impl TryFrom<ContractEvent> for RevRegEntryCreated {
@@ -34,21 +32,16 @@ impl TryFrom<ContractEvent> for RevRegEntryCreated {
         let rev_reg_entry_tuple = log.get_tuple(2)?;
         let current_accumulator = String::from_utf8(rev_reg_entry_tuple.get_bytes(0)?)
             .map_err(|e| VdrError::InvalidRevocationRegistryEntry(e.to_string()))?;
-        let prev_accumulator = String::from_utf8(rev_reg_entry_tuple.get_bytes(1)?)
-            .map_err(|e| VdrError::InvalidRevocationRegistryEntry(e.to_string()))?;
-        let issued = rev_reg_entry_tuple.get_uint32_array(2)?;
-        let revoked = rev_reg_entry_tuple.get_uint32_array(3)?;
-        let entry_timestamp = rev_reg_entry_tuple.get_u64(4)?;
+        let issued = rev_reg_entry_tuple.get_uint32_array(1)?;
+        let revoked = rev_reg_entry_tuple.get_uint32_array(2)?;
 
         Ok(RevRegEntryCreated {
             revocation_registry_definition_id,
             timestamp,
             rev_reg_entry: RevRegEntry {
                 current_accumulator,
-                prev_accumulator,
                 issued,
                 revoked,
-                timestamp: entry_timestamp,
             },
         })
     }
